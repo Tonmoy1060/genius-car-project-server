8c4d0f07336fa406865db4f1ab774b052fe43efc
Tonmoy <127950961+TonmoyGrapdes@users.noreply.github.com>
2025-07-04 15:37:43 +0600

    added code for post item and get item

--- index.js
+++ index.js
@@ -18,28 +18,48 @@ async function run() {
   try {
     await client.connect();
     const userCollection = client
-      .db("a1-steel")
-      .collection("usersData");
+      .db("a1_steel")
+      .collection("calculated_users_data", );
+    const itemCollection = client
+      .db("a1_steel")
+      .collection("item", );
 
-    app.get("/users", async (req, res) => {
+    app.get("/calculation", async (req, res) => {
       const query = {};
       const cursor = userCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
     });
+    app.get("/items", async (req, res) => {
+      const query = {};
+      const cursor = itemCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
-    app.get("/users/:id", async(req,res) => {
+    app.post('/calculation', async(req,res) => {
+        const data = req.body;
+        const result = await userCollection.insertOne(data);
+        res.send(result);
+      })
+    app.post('/items', async(req,res) => {
+        const data = req.body;
+        const result = await itemCollection.insertOne(data);
+        res.send(result);
+      })
+
+    app.get("/calculation/:id", async(req,res) => {
       const id = req.params.id;
       const query = {_id: ObjectId(id)};
       const user = await userCollection.findOne(query);
       res.send(user);
     })
-    app.post('/users', async(req,res) => {
+    app.post('/calculation', async(req,res) => {
       const data = req.body;
       const result = await userCollection.insertOne(data);
       res.send(result);
     })
-    app.delete('/users/:id', async(req,res) => {
+    app.delete('/calculation/:id', async(req,res) => {
       const id = req.params.id;
       const query = {_id: ObjectId(id)};
       const result = await userCollection.deleteOne(query);
